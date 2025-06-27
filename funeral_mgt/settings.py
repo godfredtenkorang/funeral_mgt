07d5e45b176563dcc9e7ef3ec0816a5b3aae056f
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-06-27 16:16:31 +0000

    changese

--- funeral_mgt/settings.py
+++ funeral_mgt/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2$ubn2cyumm46h4@r+u#b80lbtaye%l-ibkmu=vfh7i6_*n7x9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
